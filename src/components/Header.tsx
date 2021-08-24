f92e83540814981919f0263d1aff89892a79680c
Tijani Fulani <Fulani07@gmail.com>
2021-08-24 17:54:11 +0100

    header done

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { User } from '../App';
+import { getLogoutUser } from '../utilities/apiClient';
+
+type HeaderProps = {
+  loggedUser: User | null;
+  clearUserState: (data: null) => void;
+};
+
+const LoggedInHeader = ({
+  username,
+  clearUserState,
+}: {
+  username: string;
+  clearUserState: (data: null) => void;
+}) => {
+  return (
+    <>
+      <h3>Welcome {username}</h3>
+      <button
+        onClick={() => {
+          getLogoutUser().then((data) => clearUserState(data));
+        }}
+      >
+        Logout
+      </button>
+    </>
+  );
+};
+
+const Header = ({ loggedUser, clearUserState }: HeaderProps) => {
+  const location = useLocation();
+
+  return (
+    <header className='header'>
+      <h1>Welcome to my Auth App</h1>
+      {loggedUser ? (
+        <LoggedInHeader
+          username={loggedUser.username}
+          clearUserState={clearUserState}
+        />
+      ) : null}
+    </header>
+  );
+};
+
+export default Header;
